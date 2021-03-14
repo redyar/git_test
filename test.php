8b3ee1d012cba82e2503e35a882d157df2dac189
redyar <winterisred@gmail.com>
2021-03-14 21:47:52 +0400

    test commit

---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+print_r('Robert');mm,n
+?>
+<html> 
+    <head>
+        <meta charset="UTF-8">
+        <title>Something New!</title>
+        <link rel="stylesheet" href="../PHP/style.css">
+    </head>
+        <header>
+        <img src="/img" alt="Logo">
+        <nav class="nav-st">
+            <ul>
+                <li>My name is</li>
+                <li>My soname is</li>
+                <li>What i like eat?</li>
+            </ul>
+        </nav>
+        </header>
+
+        <main>
+        <h1 class="title-main">What i know about me?</h1>
+        <p class="red" id="blue">Hmmm... I borned in 1992 in Russia. This is the End!</p>
+        </main>
+</html>
